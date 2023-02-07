4d9d421577d3618090e08933f969f2462d7668fb
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2023-02-07 15:41:26 -0500

    Add task builders to export file

--- src/task-builders/index.js
+++ src/task-builders/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 export { default as clean } from './clean-task-builder.js';
-// export { default as format } from './format.js';
-// export { default as lint } from './lint.js';
+export { default as format } from './format-task-builder.js';
+export { default as lint } from './lint-task-builder.js';
 // export { default as build } from './build/index.js';
 // export { default as test } from './test/index.js';
 // export { default as docs } from './docs/index.js';
