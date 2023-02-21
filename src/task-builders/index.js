33b0883444f3f5a70f3125b7b50a1108af77f2b1
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2023-02-21 08:32:08 -0500

    Export watch task builder task

--- src/task-builders/index.js
+++ src/task-builders/index.js
@@ -5,6 +5,7 @@ export * from './format-task-builder.js';
 export * from './lint-task-builder.js';
 export * from './lint-fix-task-builder.js';
 export * from './build-js-task-builder.js';
+export * from './watch-task-builder.js';
 // export { default as build } from './build/index.js';
 // export { default as test } from './test/index.js';
 // export { default as docs } from './docs/index.js';
