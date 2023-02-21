290f03e1ec0b48eb15d49cdaf7fd6c3f7522c9f2
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2023-02-21 13:33:21 -0500

    Export copy task builder

--- src/task-builders/index.js
+++ src/task-builders/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
+export * from './build-js-task-builder.js';
 export * from './clean-task-builder.js';
+export * from './copy-files-task-builder.js';
 export * from './format-task-builder.js';
 export * from './lint-task-builder.js';
 export * from './lint-fix-task-builder.js';
-export * from './build-js-task-builder.js';
 export * from './watch-task-builder.js';
 // export { default as build } from './build/index.js';
 // export { default as test } from './test/index.js';
