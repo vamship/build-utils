e4b53d70cb29e39f9d0d0fb7891f9b83e6866a59
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2023-03-10 13:14:51 -0500

    Implement typescript build task

---
+++ test/unit/task-builders/build-ts-task-builder-spec.js
@@ -0,0 +1,177 @@
+import _chai, { expect } from 'chai';
+import _sinonChai from 'sinon-chai';
+_chai.use(_sinonChai);
+
+import _path from 'path';
+import _camelcase from 'camelcase';
+
+import { stub } from 'sinon';
+import _esmock from 'esmock';
+import { Project } from '../../../src/project.js';
+import {
+    getAllProjectOverrides,
+    getSelectedProjectOverrides,
+    generateGlobPatterns,
+} from '../../utils/data-generator.js';
+import {
+    buildProjectDefinition,
+    createGulpMock,
+    createModuleImporter,
+} from '../../utils/object-builder.js';
+import { injectBuilderInitTests } from '../../utils/task-builder-snippets.js';
+
+describe('[BuildTsTaskBuilder]', () => {
+    const _importModule = createModuleImporter(
+        'src/task-builders/build-ts-task-builder.js',
+        {
+            gulpMock: 'gulp',
+            gulpTypescriptMock: 'gulp-typescript',
+            taskBuilderMock: 'src/task-builder.js',
+        },
+        'BuildTsTaskBuilder'
+    );
+
+    injectBuilderInitTests(
+        _importModule,
+        'build-ts',
+        'Build typescript files and writes them to the build directory'
+    );
+
+    describe('[task]', () => {
+        async function _createTask(definitionOverrides) {
+            const gulpMock = createGulpMock();
+            const gulpTypescriptMock = {
+                createProject: stub().callsFake(
+                    () => gulpTypescriptMock.project
+                ),
+                project: stub().callsFake(() => ({
+                    _source: '_tsproject_ret_',
+                })),
+            };
+            const BuildTsTaskBuilder = await _importModule({
+                gulpMock,
+                gulpTypescriptMock,
+            });
+
+            const definition = buildProjectDefinition(definitionOverrides);
+            const project = new Project(definition);
+            const builder = new BuildTsTaskBuilder();
+
+            return {
+                gulpMock,
+                gulpTypescriptMock,
+                project,
+                task: builder.buildTask(project),
+            };
+        }
+
+        function createSourceList(project, overrides) {
+            const dirs = ['src', 'test', 'infra'];
+            const extensions = ['ts'];
+            const rootDir = project.rootDir.absolutePath;
+
+            return generateGlobPatterns(rootDir, dirs, extensions);
+        }
+
+        getAllProjectOverrides().forEach(({ title, overrides }) => {
+            describe(`Verify task (${title})`, () => {
+                it('should inititalize and set the appropriate gulp source files', async () => {
+                    const { gulpMock, task, project } = await _createTask(
+                        overrides
+                    );
+                    const files = createSourceList(project, overrides);
+
+                    expect(gulpMock.src).to.not.have.been.called;
+
+                    task();
+
+                    expect(gulpMock.src).to.have.been.calledOnce;
+                    expect(gulpMock.callSequence[0]).to.equal('src');
+                    expect(gulpMock.src.args[0]).to.have.length(2);
+                    expect(gulpMock.src.args[0][0]).to.have.members(files);
+                    expect(gulpMock.src.args[0][1]).to.deep.equal({
+                        allowEmpty: true,
+                        base: project.rootDir.globPath,
+                    });
+                });
+
+                it('should create a new instance of the gulp typescript object', async () => {
+                    const { gulpTypescriptMock, task } = await _createTask(
+                        overrides
+                    );
+
+                    expect(gulpTypescriptMock.createProject).to.not.have.been
+                        .called;
+
+                    task();
+
+                    expect(
+                        gulpTypescriptMock.createProject
+                    ).to.have.been.calledOnceWithExactly('tsconfig.json');
+                });
+
+                it('should create initialize the new typescript object to get a compiler reference', async () => {
+                    const { gulpTypescriptMock, task } = await _createTask(
+                        overrides
+                    );
+
+                    expect(gulpTypescriptMock.project).to.not.have.been.called;
+
+                    task();
+
+                    expect(
+                        gulpTypescriptMock.project
+                    ).to.have.been.calledOnceWithExactly();
+                });
+
+                it('should pipe the source files to the typescript compiler', async () => {
+                    const { gulpMock, gulpTypescriptMock, task, project } =
+                        await _createTask(overrides);
+
+                    expect(gulpMock.pipe).to.not.have.been.called;
+
+                    task();
+
+                    expect(gulpMock.dest).to.have.been.calledOnce;
+                    expect(gulpMock.callSequence[1]).to.equal('pipe');
+
+                    expect(gulpMock.pipe.args[0]).to.have.length(1);
+                    expect(gulpMock.pipe.args[0][0]).to.deep.equal(
+                        gulpTypescriptMock.project.returnValues[0]
+                    );
+                });
+
+                it('should write the source files to the working directories', async () => {
+                    const { gulpMock, task, project } = await _createTask(
+                        overrides
+                    );
+
+                    expect(gulpMock.pipe).to.not.have.been.called;
+                    expect(gulpMock.dest).to.not.have.been.called;
+
+                    task();
+
+                    expect(gulpMock.dest).to.have.been.calledOnce;
+                    expect(gulpMock.callSequence[2]).to.equal('dest');
+
+                    expect(gulpMock.dest.args[0]).to.have.length(1);
+                    expect(gulpMock.dest.args[0][0]).to.equal(
+                        _path.join(
+                            project.rootDir.absolutePath,
+                            'working',
+                            _path.sep
+                        )
+                    );
+
+                    expect(gulpMock.pipe).to.have.been.called;
+                    expect(gulpMock.callSequence[3]).to.equal('pipe');
+
+                    expect(gulpMock.pipe.args[1]).to.have.length(1);
+                    expect(gulpMock.pipe.args[1][0]).to.equal(
+                        gulpMock.dest.returnValues[0]
+                    );
+                });
+            });
+        });
+    });
+});
