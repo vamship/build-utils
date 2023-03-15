17446a9fded63274e96f971f95947a863411c4b5
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2023-03-15 13:46:31 -0400

    Implement build for UI projects

---
+++ src/task-builders/build-ui-task-builder.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import TaskBuilder from '../task-builder.js';
+import { Project } from '../project.js';
+import { execa as _execa } from 'execa';
+
+/**
+ * Builder that can be used to generate a gulp task to build a web UI project.
+ */
+export class BuildUiTaskBuilder extends TaskBuilder {
+    /**
+     * Creates a new task builder.
+     */
+    constructor() {
+        super('build-ui', 'Build web ui project');
+    }
+
+    /**
+     * Generates a gulp task to build a web UI project.
+     *
+     * @protected
+     * @param {Object} project Reference to the project for which the task needs
+     * to be defined.
+     *
+     * @returns {Function} A gulp task.
+     */
+    _createTask(project) {
+        if (!(project instanceof Project)) {
+            throw new Error('Invalid project (arg #1)');
+        }
+
+        const viteBin = project.rootDir.getFilePath('node_modules/.bin/vite');
+        const task = () => _execa(viteBin, ['build'], { stdio: 'inherit' });
+        return task;
+    }
+}
---
+++ test/unit/task-builders/build-ui-task-builder-spec.js
@@ -0,0 +1,92 @@
+import _chai, { expect } from 'chai';
+import _sinonChai from 'sinon-chai';
+_chai.use(_sinonChai);
+
+import _path from 'path';
+import _camelcase from 'camelcase';
+
+import { stub } from 'sinon';
+import _esmock from 'esmock';
+import { Project } from '../../../src/project.js';
+import {
+    getAllProjectOverrides,
+    getSelectedProjectOverrides,
+    generateGlobPatterns,
+} from '../../utils/data-generator.js';
+import {
+    buildProjectDefinition,
+    createGulpMock,
+    createModuleImporter,
+} from '../../utils/object-builder.js';
+import { injectBuilderInitTests } from '../../utils/task-builder-snippets.js';
+
+describe('[BuildUiTaskBuilder]', () => {
+    const _importModule = createModuleImporter(
+        'src/task-builders/build-ui-task-builder.js',
+        {
+            execaModuleMock: 'execa',
+            taskBuilderMock: 'src/task-builder.js',
+        },
+        'BuildUiTaskBuilder'
+    );
+
+    injectBuilderInitTests(_importModule, 'build-ui', 'Build web ui project');
+
+    describe('[task]', () => {
+        async function _createTask(definitionOverrides) {
+            const execaModuleMock = {
+                execa: stub().callsFake(() => ({
+                    source: '_execa_ret_',
+                })),
+            };
+            const BuildUiTaskBuilder = await _importModule({
+                execaModuleMock,
+            });
+
+            const definition = buildProjectDefinition(definitionOverrides);
+            const project = new Project(definition);
+            const builder = new BuildUiTaskBuilder();
+
+            return {
+                execaModuleMock,
+                project,
+                task: builder.buildTask(project),
+            };
+        }
+
+        function createSourceList(project, overrides) {
+            const dirs = ['src', 'test', 'infra'];
+            const extensions = ['ts'];
+            const rootDir = project.rootDir.absolutePath;
+
+            return generateGlobPatterns(rootDir, dirs, extensions);
+        }
+
+        getAllProjectOverrides(1).forEach(({ title, overrides }) => {
+            describe(`Verify task (${title})`, () => {
+                it('should invoke vite to build the web project', async () => {
+                    const {
+                        execaModuleMock: { execa: execaMock },
+                        project,
+                        task,
+                    } = await _createTask(overrides);
+
+                    expect(execaMock).to.not.have.been.called;
+
+                    task();
+
+                    expect(execaMock).to.have.been.calledOnceWithExactly(
+                        _path.join(
+                            project.rootDir.absolutePath,
+                            'node_modules',
+                            '.bin',
+                            'vite'
+                        ),
+                        ['build'],
+                        { stdio: 'inherit' }
+                    );
+                });
+            });
+        });
+    });
+});
