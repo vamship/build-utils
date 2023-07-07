41adbd31333a00f6cdf1ab5afba0108395abcd1f
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2023-07-07 12:28:22 -0400

    Export additional task builders

--- src/task-builders/index.js
+++ src/task-builders/index.js
@@ -1,14 +1,21 @@
 'use strict';
 
 export * from './build-js-task-builder.js';
+export * from './build-ts-task-builder.js';
+export * from './build-ui-task-builder.js';
 export * from './clean-task-builder.js';
 export * from './copy-files-task-builder.js';
+export * from './docs-js-task-builder.js';
+export * from './docs-ts-task-builder.js';
 export * from './format-task-builder.js';
-export * from './lint-task-builder.js';
 export * from './lint-fix-task-builder.js';
+export * from './lint-task-builder.js';
+export * from './package-aws-task-builder.js';
+export * from './package-container-task-builder.js';
+export * from './package-npm-task-builder.js';
+export * from './publish-aws-task-builder.js';
+export * from './publish-container-task-builder.js';
+export * from './publish-npm-task-builder.js';
+export * from './test-task-builder.js';
+export * from './test-ui-task-builder.js';
 export * from './watch-task-builder.js';
-// export { default as build } from './build/index.js';
-// export { default as test } from './test/index.js';
-// export { default as docs } from './docs/index.js';
-// export { default as package } from './package/index.js';
-// export { default as publish } from './publish/index.js';
