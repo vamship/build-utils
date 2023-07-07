9877446faaa5a08bb84ae8338254255a8a0ff9b4
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2023-07-07 11:57:12 -0400

    Implement typescript doc generator

---
+++ src/task-builders/ts-doc-task-builder.js
@@ -0,0 +1,64 @@
+import _path from 'path';
+import _gulp from 'gulp';
+import _gulpTypedoc from 'gulp-typedoc';
+
+import TaskBuilder from '../task-builder.js';
+import { Project } from '../project.js';
+
+/**
+ * Builder that can be used to generate a gulp task to generate documentation
+ * from code comments in typescript files.
+ */
+export class TsDocTaskBuilder extends TaskBuilder {
+    /**
+     * Creates a new task builder.
+     */
+    constructor() {
+        super(
+            'ts-doc',
+            'Generates documentation from code comments in typescript files'
+        );
+    }
+
+    /**
+     * Generates a gulp task to generate documentation from code comments in
+     * source code.
+     *
+     * @protected
+     * @param {Object} project Reference to the project for which the task needs
+     * to be defined.
+     *
+     * @returns {Function} A gulp task.
+     */
+    _createTask(project) {
+        if (!(project instanceof Project)) {
+            throw new Error('Invalid project (arg #1)');
+        }
+
+        const { rootDir, name, version } = project;
+        const docsDir = rootDir.getChild('docs');
+
+        const dirs = ['src'];
+        const extensions = ['ts'];
+        const options = {
+            name: `${project.name} Documentation`,
+            disableOutputCheck: true,
+            readme: rootDir.getFilePath('README.md'),
+            out: docsDir.getFilePath(`${name}${_path.sep}${version}`),
+        };
+
+        const paths = dirs
+            .map((dir) => project.rootDir.getChild(dir))
+            .map((dir) => extensions.map((ext) => dir.getAllFilesGlob(ext)))
+            .reduce((result, arr) => result.concat(arr), []);
+
+        const task = () =>
+            _gulp
+                .src(paths, {
+                    allowEmpty: true,
+                    base: project.rootDir.globPath,
+                })
+                .pipe(_gulpTypedoc(options));
+        return task;
+    }
+}
---
+++ test/unit/task-builders/ts-doc-task-builder-spec.js
@@ -0,0 +1,129 @@
+import _chai, { expect } from 'chai';
+import _sinonChai from 'sinon-chai';
+_chai.use(_sinonChai);
+
+import _path from 'path';
+import { stub } from 'sinon';
+import _esmock from 'esmock';
+import { Project } from '../../../src/project.js';
+import {
+    getAllProjectOverrides,
+    generateGlobPatterns,
+} from '../../utils/data-generator.js';
+import {
+    buildProjectDefinition,
+    createModuleImporter,
+    createGulpMock,
+} from '../../utils/object-builder.js';
+import { injectBuilderInitTests } from '../../utils/task-builder-snippets.js';
+
+describe('[TsDocTaskBuilder]', () => {
+    const _importModule = createModuleImporter(
+        'src/task-builders/ts-doc-task-builder.js',
+        {
+            gulpMock: 'gulp',
+            gulpTypeDocMock: 'gulp-typedoc',
+            taskBuilderMock: 'src/task-builder.js',
+        },
+        'TsDocTaskBuilder'
+    );
+
+    injectBuilderInitTests(
+        _importModule,
+        'ts-doc',
+        'Generates documentation from code comments in typescript files'
+    );
+
+    describe('[task]', () => {
+        async function _createTask(definitionOverrides) {
+            const gulpTypeDocMock = stub().callsFake(() => ({
+                _source: '_typeodc_ret_',
+            }));
+            const gulpMock = createGulpMock();
+
+            const TsDocTaskBuilder = await _importModule({
+                gulpMock,
+                gulpTypeDocMock,
+            });
+
+            const definition = buildProjectDefinition(definitionOverrides);
+            const project = new Project(definition);
+            const builder = new TsDocTaskBuilder();
+
+            return {
+                gulpMock,
+                gulpTypeDocMock,
+                project,
+                task: builder.buildTask(project),
+            };
+        }
+
+        function createSourceList(project, overrides) {
+            const dirs = ['src'];
+            const extensions = ['ts'];
+            const rootDir = project.rootDir.absolutePath;
+
+            return generateGlobPatterns(rootDir, dirs, extensions);
+        }
+
+        getAllProjectOverrides().forEach(({ title, overrides }) => {
+            describe(`Verify task (${title})`, () => {
+                it('should inititalize and set the appropriate gulp source files', async () => {
+                    const { gulpMock, task, project } = await _createTask(
+                        overrides
+                    );
+                    const files = createSourceList(project, overrides);
+
+                    expect(gulpMock.src).to.not.have.been.called;
+
+                    task();
+
+                    expect(gulpMock.src).to.have.been.calledOnce;
+                    expect(gulpMock.callSequence[0]).to.equal('src');
+
+                    expect(gulpMock.src.args[0]).to.have.length(2);
+                    expect(gulpMock.src.args[0][0]).to.have.members(files);
+                    expect(gulpMock.src.args[0][1]).to.deep.equal({
+                        allowEmpty: true,
+                        base: project.rootDir.globPath,
+                    });
+                });
+
+                it('should pipe the source files to the document generator to extract docs', async () => {
+                    const { gulpMock, task, gulpTypeDocMock, project } =
+                        await _createTask(overrides);
+
+                    expect(gulpMock.pipe).to.not.have.been.called;
+                    expect(gulpTypeDocMock).to.not.have.been.called;
+
+                    task();
+
+                    expect(gulpTypeDocMock).to.have.been.calledOnce;
+                    expect(gulpTypeDocMock.args[0]).to.have.length(1);
+                    expect(gulpTypeDocMock.args[0][0]).to.deep.equal({
+                        name: `${project.name} Documentation`,
+                        disableOutputCheck: true,
+                        readme: _path.join(
+                            project.rootDir.absolutePath,
+                            'README.md'
+                        ),
+                        out: _path.join(
+                            project.rootDir.absolutePath,
+                            'docs',
+                            project.name,
+                            project.version
+                        ),
+                    });
+
+                    expect(gulpMock.pipe).to.have.been.called;
+                    expect(gulpMock.callSequence[1]).to.equal('pipe');
+
+                    expect(gulpMock.pipe.args[0]).to.have.length(1);
+                    expect(gulpMock.pipe.args[0][0]).to.equal(
+                        gulpTypeDocMock.returnValues[0]
+                    );
+                });
+            });
+        });
+    });
+});
