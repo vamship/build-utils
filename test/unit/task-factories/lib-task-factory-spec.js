09c42e607ba79bf8b72ef9b2f0cff5e290e67813
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2023-08-11 16:40:23 -0400

    Factor out reusable test snippets

--- test/unit/task-factories/lib-task-factory-spec.js
+++ test/unit/task-factories/lib-task-factory-spec.js
@@ -18,7 +18,11 @@ import {
     getAllProjectOverrides,
     getSelectedProjectOverrides,
 } from '../../utils/data-generator.js';
-import { injectFactoryInitTests } from '../../utils/task-factory-snippets.js';
+import {
+    injectFactoryInitTests,
+    injectUnsupportedTasksTests,
+    injectTaskBuilderCompositionTests,
+} from '../../utils/task-factory-snippets.js';
 
 describe('[LibTaskFactory]', () => {
     const _builderNames = [
@@ -69,6 +73,7 @@ describe('[LibTaskFactory]', () => {
     }
 
     describe('_createTaskBuilders()', () => {
+        const PROJECT_TYPE = 'lib';
         async function _createFactory(overrides) {
             const { mocks, mockReferences } =
                 createTaskBuilderImportMocks(_builderNames);
@@ -80,112 +85,12 @@ describe('[LibTaskFactory]', () => {
             return { project, factory, mocks };
         }
 
-        function _createCtorNotCalledChecker(overrides) {
-            return (mock) => {
-                const failMessage = buildFailMessage(overrides, {
-                    task: mock._name,
-                });
-                expect(mock.ctor, failMessage).not.to.have.been.called;
-            };
-        }
+        injectUnsupportedTasksTests(PROJECT_TYPE, _createFactory);
 
-        describe(`[Unsupported Project Types]`, () => {
-            getAllProjectOverrides()
-                .filter(({ overrides }) => {
-                    return overrides['buildMetadata.type'] !== 'lib';
-                })
-                .forEach(({ title, overrides }) => {
-                    const checkCtorNotCalled =
-                        _createCtorNotCalledChecker(overrides);
-
-                    it(`should not initialize any task builders for unsupported project types (${title})`, async () => {
-                        const { factory, mocks } = await _createFactory(
-                            overrides
-                        );
-
-                        Object.values(mocks).forEach(checkCtorNotCalled);
-
-                        factory._createTaskBuilders();
-
-                        Object.values(mocks).forEach(checkCtorNotCalled);
-                    });
-
-                    it(`should return an empty task list for unsupported project types (${title})`, async () => {
-                        const { factory } = await _createFactory(overrides);
-                        const ret = factory._createTaskBuilders();
-                        const failMessage = buildFailMessage(overrides);
-
-                        expect(ret, failMessage).to.be.an('Array').and.to.be
-                            .empty;
-                    });
-                });
-        });
-
-        getSelectedProjectOverrides(['lib']).forEach(({ title, overrides }) => {
-            const checkCtorNotCalled = _createCtorNotCalledChecker(overrides);
-
-            describe(`[${title}]`, () => {
-                it(`should initialize the expected task builders`, async () => {
-                    const { project, factory, mocks } = await _createFactory(
-                        overrides
-                    );
-
-                    const expectedBuilders = _getExpectedTaskBuilders(project);
-
-                    Object.values(mocks).forEach(checkCtorNotCalled);
-
-                    factory._createTaskBuilders();
-
-                    Object.keys(mocks).forEach((subBuilderMock) => {
-                        const mock = mocks[subBuilderMock];
-                        const builder = expectedBuilders.find(
-                            (builder) => builder.name === mock._name
-                        );
-                        const ctor = mock.ctor;
-                        const failMessage = buildFailMessage(overrides, {
-                            task: mock._name,
-                        });
-
-                        if (builder) {
-                            expect(ctor, failMessage).to.have.been.calledOnce;
-                            expect(
-                                ctor,
-                                failMessage
-                            ).to.have.been.calledWithExactly(
-                                ...builder.ctorArgs
-                            );
-                            expect(ctor, failMessage).to.have.been
-                                .calledWithNew;
-                        } else {
-                            expect(ctor, failMessage).to.not.have.been.called;
-                        }
-                    });
-                });
-
-                it(`should return the expected task builders`, async () => {
-                    const { project, factory, mocks } = await _createFactory(
-                        overrides
-                    );
-                    const failMessage = buildFailMessage(overrides);
-
-                    const expectedBuilders = _getExpectedTaskBuilders(project);
-
-                    const ret = factory._createTaskBuilders();
-
-                    expect(ret, failMessage)
-                        .to.be.an('Array')
-                        .and.to.have.length(expectedBuilders.length);
-
-                    expectedBuilders.forEach((builder, index) => {
-                        const mock = mocks[builder.name];
-                        const failMessage = buildFailMessage(overrides, {
-                            builder: builder.name,
-                        });
-
-                        expect(ret, failMessage).to.include(mock);
-                    });
-                });
-            });
-        });
+        injectTaskBuilderCompositionTests(
+            PROJECT_TYPE,
+            _createFactory,
+            _getExpectedTaskBuilders
+        );
     });
 });
--- test/utils/task-factory-snippets.js
+++ test/utils/task-factory-snippets.js
@@ -5,6 +5,27 @@ _chai.use(_sinonChai);
 import { spy } from 'sinon';
 import { getAllButObject } from './data-generator.js';
 import { buildFailMessage } from './object-builder.js';
+import {
+    getAllProjectOverrides,
+    getSelectedProjectOverrides,
+} from './data-generator.js';
+
+/**
+ * Returns a checker function that checks that the mock's constructor was never
+ * called.
+ *
+ * @param {Object} overrides An object that contains overrides for the test
+ * case.
+ * @returns {Function} A function that performs the check.
+ */
+function _createCtorNotCalledChecker(overrides) {
+    return (mock) => {
+        const failMessage = buildFailMessage(overrides, {
+            task: mock._name,
+        });
+        expect(mock.ctor, failMessage).not.to.have.been.called;
+    };
+}
 
 /**
  * Injects default tests that apply to all task factories. This is a utility
@@ -55,4 +76,127 @@ export function injectFactoryInitTests(importModule, project, ctorArgs) {
             );
         });
     });
+}
+
+/**
+ * Injects tests that check that the task factory does not return any tasks for
+ * unsupported project types.
+ *
+ * @param {String} projectType The project type that the task factory applies
+ * to. All other project types are considered unsupported.
+ * @param {Function} createFactory A function that can be used to initialize a
+ * factory with.
+ */
+export function injectUnsupportedTasksTests(projectType, createFactory) {
+    const projectOverrides = getAllProjectOverrides().filter(
+        ({ overrides }) => overrides['buildMetadata.type'] !== projectType
+    );
+
+    describe(`[Unsupported Project Types]`, () => {
+        projectOverrides.forEach(({ title, overrides }) => {
+            const checkCtorNotCalled = _createCtorNotCalledChecker(overrides);
+
+            it(`should not initialize any task builders for unsupported project types (${title})`, async () => {
+                const { factory, mocks } = await createFactory(overrides);
+
+                Object.values(mocks).forEach(checkCtorNotCalled);
+                factory._createTaskBuilders();
+                Object.values(mocks).forEach(checkCtorNotCalled);
+            });
+
+            it(`should return an empty task list for unsupported project types (${title})`, async () => {
+                const { factory } = await createFactory(overrides);
+                const ret = factory._createTaskBuilders();
+                const failMessage = buildFailMessage(overrides);
+
+                expect(ret, failMessage).to.be.an('Array').and.to.be.empty;
+            });
+        });
+    });
+}
+
+/**
+ * Injects tests that check the composition of the subtasks included by the
+ * factory.
+ *
+ * @param {String} projectType The project type that the task factory applies
+ * to.
+ * @param {Function} createFactory A function that can be used to initialize a
+ * factory with.
+ * @param {Function} getExpectedTaskBuilders A function that returns the list
+ * of expected task builders for the factory.
+ */
+export function injectTaskBuilderCompositionTests(
+    projectType,
+    createFactory,
+    getExpectedTaskBuilders
+) {
+    getSelectedProjectOverrides([projectType]).forEach(
+        ({ title, overrides }) => {
+            const checkCtorNotCalled = _createCtorNotCalledChecker(overrides);
+
+            describe(`[${title}]`, () => {
+                it(`should initialize the expected task builders`, async () => {
+                    const { project, factory, mocks } = await createFactory(
+                        overrides
+                    );
+
+                    const expectedBuilders = getExpectedTaskBuilders(project);
+
+                    Object.values(mocks).forEach(checkCtorNotCalled);
+
+                    factory._createTaskBuilders();
+
+                    Object.keys(mocks).forEach((subBuilderMock) => {
+                        const mock = mocks[subBuilderMock];
+                        const builder = expectedBuilders.find(
+                            (builder) => builder.name === mock._name
+                        );
+                        const ctor = mock.ctor;
+                        const failMessage = buildFailMessage(overrides, {
+                            task: mock._name,
+                        });
+
+                        if (builder) {
+                            expect(ctor, failMessage).to.have.been.calledOnce;
+                            expect(
+                                ctor,
+                                failMessage
+                            ).to.have.been.calledWithExactly(
+                                ...builder.ctorArgs
+                            );
+                            expect(ctor, failMessage).to.have.been
+                                .calledWithNew;
+                        } else {
+                            expect(ctor, failMessage).to.not.have.been.called;
+                        }
+                    });
+                });
+
+                it(`should return the expected task builders`, async () => {
+                    const { project, factory, mocks } = await createFactory(
+                        overrides
+                    );
+                    const failMessage = buildFailMessage(overrides);
+
+                    const expectedBuilders = getExpectedTaskBuilders(project);
+
+                    const ret = factory._createTaskBuilders();
+
+                    expect(ret, failMessage)
+                        .to.be.an('Array')
+                        .and.to.have.length(expectedBuilders.length);
+
+                    expectedBuilders.forEach((builder, index) => {
+                        const mock = mocks[builder.name];
+                        const failMessage = buildFailMessage(overrides, {
+                            builder: builder.name,
+                        });
+
+                        expect(ret, failMessage).to.include(mock);
+                    });
+                });
+            });
+        }
+    );
 }
