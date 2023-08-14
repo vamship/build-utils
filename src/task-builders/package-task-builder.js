8d301c7de44bfc24c3783697709700bed8af14be
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2023-08-14 15:06:00 -0400

    WIP: Create composite package builder

---
+++ src/task-builders/package-task-builder.js
@@ -0,0 +1,72 @@
+'use strict';
+
+import _gulp from 'gulp';
+import TaskBuilder from '../task-builder.js';
+import { Project } from '../project.js';
+import { NotSupportedTaskBuilder } from './not-supported-task-builder.js';
+import { PackageAwsTaskBuilder } from './package-aws-task-builder.js';
+import { PackageNpmTaskBuilder } from './package-npm-task-builder.js';
+import { PackageContainerTaskBuilder } from './package-container-task-builder.js';
+
+/**
+ * General purpose packaging task that configures sub tasks for packaging based
+ * on the project. When a project is a container and has multiple container
+ * targets, this task will either package the target named "default" or the
+ * first target defined for the project.
+ */
+export class PackageTaskBuilder extends TaskBuilder {
+    /**
+     * Creates a new task builder.
+     */
+    constructor() {
+        super(
+            'package',
+            `Packages project build files for publishing to a repository`
+        );
+    }
+
+    /**
+     * Generates a gulp task to package a project for packaging a project.
+     *
+     * @protected
+     * @param {Object} project Reference to the project for which the task needs
+     * to be defined.
+     *
+     * @returns {Function} A gulp task.
+     */
+    _createTask(project) {
+        if (!(project instanceof Project)) {
+            throw new Error('Invalid project (arg #1)');
+        }
+
+        const builders = this._getSubBuilders(project).map((builder) =>
+            builder.buildTask(project)
+        );
+
+        const task = _gulp.series(builders);
+        return task;
+    }
+
+    /**
+     * Returns a list of sub builders based on the project type and language.
+     * @private
+     */
+    _getSubBuilders(project) {
+        const { type, language } = project;
+        if (type === 'lib') {
+            return [new PackageNpmTaskBuilder()];
+        } else if (type === 'aws-microservice') {
+            return [new PackageAwsTaskBuilder()];
+        }
+        return [new NotSupportedTaskBuilder()];
+        // if (type === 'container') {
+        //     return [new NotSupportedTaskBuilder()];
+        // } else if (language === 'ts') {
+        //     return [new PackageNpmTaskBuilder()];
+        // } else if (language === 'ts') {
+        //     return [new PackageContainerTaskBuilder()];
+        // } else {
+        //     return [new PackageNpmTaskBuilder()];
+        // }
+    }
+}
---
+++ test/unit/task-builders/package-task-builder-spec.js
@@ -0,0 +1,96 @@
+import _chai, { expect } from 'chai';
+import _sinonChai from 'sinon-chai';
+_chai.use(_sinonChai);
+
+import _path from 'path';
+
+import { stub, spy } from 'sinon';
+import _esmock from 'esmock';
+import { Project } from '../../../src/project.js';
+import {
+    getAllButString,
+    getAllProjectOverrides,
+    generateGlobPatterns,
+} from '../../utils/data-generator.js';
+import {
+    buildProjectDefinition,
+    createGulpMock,
+    createFancyLogMock,
+    createModuleImporter,
+    createTaskBuilderMock,
+    createTaskBuilderImportDefinitions,
+    createTaskBuilderImportMocks,
+} from '../../utils/object-builder.js';
+import {
+    injectBuilderInitTests,
+    injectSubBuilderCompositionTests,
+} from '../../utils/task-builder-snippets.js';
+
+describe('[PackageTaskBuilder]', () => {
+    const _subBuilders = [
+        'package-npm',
+        'package-aws',
+        'package-container',
+        'not-supported',
+    ];
+    const _importDefinitions = createTaskBuilderImportDefinitions(_subBuilders);
+    const _importModule = createModuleImporter(
+        'src/task-builders/package-task-builder.js',
+        {
+            taskBuilderMock: 'src/task-builder.js',
+            gulpMock: 'gulp',
+            ..._importDefinitions,
+        },
+        'PackageTaskBuilder'
+    );
+
+    async function _initializeTask() {
+        const gulpMock = createGulpMock();
+        const { mocks, mockReferences } =
+            createTaskBuilderImportMocks(_subBuilders);
+
+        const TaskBuilder = await _importModule({
+            gulpMock,
+            ...mockReferences,
+        });
+        const builder = new TaskBuilder();
+
+        return {
+            builder,
+            gulpMock,
+            subBuilderMocks: mocks,
+        };
+    }
+
+    function _getExpectedSubBuilders(project) {
+        const { type, language } = project;
+        if(type === 'ui') {
+            return [{ name: 'not-supported', ctorArgs: [] }];
+        } else if (type === 'lib') {
+            return [{ name: 'package-npm', ctorArgs: [] }];
+        } else if (type === 'aws-microservice') {
+           return [ { name: 'package-aws', ctorArgs: [] } ];
+        } 
+        return [{ name: 'not-supported', ctorArgs: [] }];
+
+        // if (type === 'ui') {
+        //     return [{ name: 'not-supported', ctorArgs: [] }];
+        // } else if (type === 'aws-microservice') {
+        //     return [
+        //         { name: 'package-aws', ctorArgs: [] },
+        //     ];
+        // } else if (project.getContainerTargets().length > 0) {
+        //     return [
+        //         { name: 'package-container', ctorArgs: [] },
+        //     ];
+        // }
+    }
+
+    injectBuilderInitTests(
+        _importModule,
+        'package',
+        `Packages project build files for publishing to a repository`
+    );
+
+    injectSubBuilderCompositionTests(_initializeTask, _getExpectedSubBuilders);
+});
