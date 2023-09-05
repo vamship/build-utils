196ffa51e785cba471802c73399f77ac3c2483fb
Jackson Saia <55666794+jacksonsaia-eng@users.noreply.github.com>
2023-09-05 13:41:06 -0400

    Adding ui task factory

--- src/task-factories/index.js
+++ src/task-factories/index.js
@@ -3,5 +3,6 @@
 export * from './lib-task-factory.js';
 export * from './api-task-factory.js';
 export * from './cli-task-factory.js';
+export * from './ui-task-factory.js';
 export * from './container-task-factory.js';
 export * from './aws-task-factory.js';
---
+++ test/unit/task-factories/ui-task-factory-spec.js
@@ -0,0 +1,79 @@
+import _chai from 'chai';
+import _sinonChai from 'sinon-chai';
+_chai.use(_sinonChai);
+
+import { UiTaskFactory } from '../../../src/task-factories/ui-task-factory.js';
+import { Project } from '../../../src/project.js';
+import {
+    buildProjectDefinition,
+    createModuleImporter,
+    createTaskBuilderImportDefinitions,
+    createTaskBuilderImportMocks,
+} from '../../utils/object-builder.js';
+
+import {
+    injectFactoryInitTests,
+    injectUnsupportedTasksTests,
+    injectTaskBuilderCompositionTests,
+} from '../../utils/task-factory-snippets.js';
+
+describe('[UiTaskFactory]', () => {
+    const _builderNames = [
+        'clean',
+        'format',
+        'lint',
+        'lint-fix',
+        'docs',
+        'build',
+    ];
+    const importDefinitions = createTaskBuilderImportDefinitions(_builderNames);
+
+    const _importModule = createModuleImporter(
+        'src/task-factories/ui-task-factory.js',
+        {
+            taskFactoryMock: 'src/task-factory.js',
+            ...importDefinitions,
+        },
+        'UiTaskFactory'
+    );
+
+    injectFactoryInitTests(
+        _importModule,
+        new Project(buildProjectDefinition())
+    );
+
+    function _getExpectedTaskBuilders(project) {
+        const builders = [
+            { name: 'clean', ctorArgs: [] },
+            { name: 'format', ctorArgs: [] },
+            { name: 'lint', ctorArgs: [] },
+            { name: 'lint-fix', ctorArgs: [] },
+            { name: 'docs', ctorArgs: [project] },
+            { name: 'build', ctorArgs: [project] },
+        ];
+
+        return builders;
+    }
+
+    describe('_createTaskBuilders()', () => {
+        const PROJECT_TYPE = 'ui';
+        async function _createFactory(overrides) {
+            const { mocks, mockReferences } =
+                createTaskBuilderImportMocks(_builderNames);
+            const TaskFactory = await _importModule(mockReferences);
+            const definition = buildProjectDefinition(overrides);
+            const project = new Project(definition);
+            const factory = new TaskFactory(project);
+
+            return { project, factory, mocks };
+        }
+
+        injectUnsupportedTasksTests(PROJECT_TYPE, _createFactory);
+
+        injectTaskBuilderCompositionTests(
+            PROJECT_TYPE,
+            _createFactory,
+            _getExpectedTaskBuilders
+        );
+    });
+});
