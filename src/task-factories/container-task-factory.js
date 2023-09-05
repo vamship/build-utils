4f3b5ba821a9cc28e722eaff481558b5baeb0da0
Jackson Saia <55666794+jacksonsaia-eng@users.noreply.github.com>
2023-09-05 13:14:15 -0400

    Adding container task factory

---
+++ src/task-factories/container-task-factory.js
@@ -0,0 +1,50 @@
+import TaskFactory from '../task-factory.js';
+
+import { CleanTaskBuilder } from '../task-builders/clean-task-builder.js';
+import { FormatTaskBuilder } from '../task-builders/format-task-builder.js';
+import { LintTaskBuilder } from '../task-builders/lint-task-builder.js';
+import { LintFixTaskBuilder } from '../task-builders/lint-fix-task-builder.js';
+import { BuildTaskBuilder } from '../task-builders/build-task-builder.js';
+import { PackageTaskBuilder } from '../task-builders/package-task-builder.js';
+import { PublishTaskBuilder } from '../task-builders/publish-task-builder.js';
+import { DocsTaskBuilder } from '../task-builders/docs-task-builder.js';
+
+/**
+ * Represents a factory that generates a set of build tasks for a given project
+ * type. This is an abstract class that must be extended to provide a list of
+ * task builders for a given project type.
+ */
+export class ContainerTaskFactory extends TaskFactory {
+    /**
+     * Creates a new instance of TaskFactory, initialized for a given project.
+     * @param {Project} project The project to generate build tasks for.
+     */
+    constructor(project) {
+        super(project);
+    }
+
+    /**
+     * Protected abstract method that can be overridden to provide the
+     * necessary tasks for a given project type.
+     *
+     * @returns {Array} An array of task builders.
+     */
+    _createTaskBuilders() {
+        const { type } = this._project;
+        if (type !== 'container') {
+            return [];
+        }
+
+        return [
+            new CleanTaskBuilder(),
+            new FormatTaskBuilder(),
+            new LintTaskBuilder(),
+            new LintFixTaskBuilder(),
+
+            new DocsTaskBuilder(this._project),
+            new BuildTaskBuilder(this._project),
+            new PackageTaskBuilder(this._project),
+            new PublishTaskBuilder(this._project),
+        ];
+    }
+}
--- src/task-factories/index.js
+++ src/task-factories/index.js
@@ -3,3 +3,4 @@
 export * from './lib-task-factory.js';
 export * from './api-task-factory.js';
 export * from './cli-task-factory.js';
+export * from './container-task-factory.js';
