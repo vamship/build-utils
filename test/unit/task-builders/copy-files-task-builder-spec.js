b24564cba96e47a1a718df9c9b2591dc13d66960
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2026-01-30 14:24:52 -0500

    Fixed failing tests

--- test/unit/task-builders/copy-files-task-builder-spec.js
+++ test/unit/task-builders/copy-files-task-builder-spec.js
@@ -222,6 +222,7 @@ describe('[CopyFilesTaskBuilder]', function () {
                     expect(gulpMock.src.args[0][1]).to.deep.equal({
                         allowEmpty: true,
                         base: project.rootDir.globPath,
+                        encoding: false,
                     });
                 });
 
